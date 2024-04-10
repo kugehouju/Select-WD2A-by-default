3af167149db3eecbc86c7f64fec528a938ecc5fd
kugehouju <kugehouju@gmail.com>
2024-04-10 10:35:17 +0900

    @namespaceを書き違えていたため変更しました。

--- src/change.user.js
+++ src/change.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Select WD2A by default
-// @namespace    https://github.com/kugehouju/Change-to-WD2A
+// @namespace    https://github.com/kugehouju/src/Change-to-WD2A
 // @version      0.1
 // @description  Select WD2A by default in the dropdown
 // @author       Houju Kuge
