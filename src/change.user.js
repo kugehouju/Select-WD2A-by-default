222d18164274c1b30358e953e35b99d8bbdbe594
kugehouju <kugehouju@gmail.com>
2024-04-24 16:45:04 +0900

    画像のリンクが切れたので修正をした気がします

--- src/change.user.js
+++ src/change.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Select WD2A by default
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  時間割ページにてドロップダウンメニューの初期値をWD2Aに設定します。
 // @author       Houju Kuge
 // @match        https://click.ecc.ac.jp/ecc/rtakimoto/schedule/
 // @match        https://click.ecc.ac.jp/ecc/rtakimoto/schedule/index.php
-// @icon         https://cdn.discordapp.com/attachments/1217178760218935336/1227125288564035615/7f3f7985e9f1ec5a.png?ex=6627447b&is=6614cf7b&hm=8f031607aed499a6710df95b6b2a6fb84fcb1b99c541974ea4f1213e82b77edc&
+// @icon         glitterHonda.png
 // @updateURL    https://github.com/kugehouju/Select-WD2A-by-default/raw/main/src/change.user.js
 // @downloadURL  https://github.com/kugehouju/Select-WD2A-by-default/raw/main/src/change.user.js
 // @grant        none
@@ -15,7 +15,7 @@
 (function() {
     'use strict';
     // カーソルストーカーの設定
-    var imageUrl = "https://cdn.discordapp.com/attachments/1217178760218935336/1227125288564035615/7f3f7985e9f1ec5a.png?ex=6627447b&is=6614cf7b&hm=8f031607aed499a6710df95b6b2a6fb84fcb1b99c541974ea4f1213e82b77edc&";
+    var imageUrl = "glitterHonda.png";
     var cursorStalker = document.createElement('img');
     cursorStalker.src = imageUrl;
     cursorStalker.style.position = 'absolute';
@@ -46,7 +46,7 @@
         selectElement.dispatchEvent(new Event('change'));
         document.querySelector('header').remove()
 
-        document.body.style.background = "url('https://cdn.discordapp.com/attachments/1217178760218935336/1227125287985086474/HONDA_CORE_wallpaper.png?ex=6627447b&is=6614cf7b&hm=d63173ebb90b83c31be377c01b34da42003fb8c5665a759dd358698571b1d4ee&') no-repeat red center / cover"
+        document.body.style.background = "url('scheduleBg.png') no-repeat red center / cover"
         document.querySelectorAll('.scheduleTable__td, p ,span , h1, h2, h3, th, td').forEach((a) => {
             a.style.color = ("white")
         })
